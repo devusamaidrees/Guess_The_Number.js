3105821a8b19c58e5aee54e6101989106c534d18
Usama Idrees <usamaidrees0096@gmail.com>
2023-04-05 23:52:19 +0500

    Adding new file

---
+++ script.js
@@ -0,0 +1,23 @@
+const prompt=require("prompt-sync") ({sigint:true})
+let n
+let a=Math.floor(Math.random() * 100);
+let Attempt=0
+console.log("The number is :",a)
+while(true)
+{
+    n=prompt("Enter a number :")
+    n=Number.parseInt(n)
+if(a>n){
+    console.log("Number is greater than :",n)
+}
+else if(a<n)
+{
+    console.log("Number is samller than :",n)
+}
+else{
+    console.log("The number is :",a)
+    console.log("Your answer is correct after "+Attempt+" Attempts")
+    return false
+}
+Attempt++;
+}
