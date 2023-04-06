77455c5e8c667ecc23fdaecd44b41bb0f8950d86
devusamaidrees <113791111+devusamaidrees@users.noreply.github.com>
2023-04-06 17:09:34 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const prompt=require("prompt-sync") ({sigint:true})
 let n
 let a=Math.floor(Math.random() * 100);
 let Attempt=0
-console.log("The number is :",a)
 while(true)
 {
     n=prompt("Enter a number :")
